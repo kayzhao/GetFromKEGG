532cd7aa2d76c913710089447e9f0bbdc66c59e8
kayzhao <kay.zkj.233@gmail.com>
2015-07-02 09:51:31 +0800

    fetch the formula,mass,weight

--- src/GetDrugsFromKEGG.java
+++ src/GetDrugsFromKEGG.java
@@ -1,10 +1,6 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.text.DecimalFormat;
 
 import org.jsoup.Jsoup;
@@ -16,8 +12,7 @@ public class GetDrugsFromKEGG {
 	public static void main(String[] args) {
 		GetDrugsFromKEGG kegg = new GetDrugsFromKEGG();
 		try {
-			kegg.getDrugs("Kegg_Drugs_Info.txt");
-			// System.out.println(getContentPr("http://www.kegg.jp/dbget-bin/www_bget?dr:D10350"));
+			kegg.getDrugs("Kegg_Drugs_Formula_Mass_Weight.txt");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -34,6 +29,9 @@ public class GetDrugsFromKEGG {
 		String result = "";
 		try {
 			doc = Jsoup.connect(url).timeout(10000).get();
+			if (doc == null) {
+				return result;
+			}
 			Elements tbodys = doc.getElementsByTag("tbody");
 			Element tbody = null;
 			if (tbodys.size() > 2) {
@@ -44,15 +42,43 @@ public class GetDrugsFromKEGG {
 			Elements nobrs = tbody.getElementsByTag("nobr");
 			for (Element nobr : nobrs) {
 				String nobr_str = nobr.ownText();
-				//
-				if (nobr_str.equals("Name")) {
+				// 获取drug name
+				// if (nobr_str.equals("Name")) {
+				// Element other_tr = nobr.parent().nextElementSibling();
+				// Elements div_tags = other_tr.getElementsByTag("div");
+				// String[] names = div_tags.get(1).ownText().split("<br>");
+				// for (String name : names) {
+				// result += name;
+				// }
+				// }
+
+				// 获取Formula
+				if (nobr_str.equals("Formula")) {
+					result += "\t";
 					Element other_tr = nobr.parent().nextElementSibling();
 					Elements div_tags = other_tr.getElementsByTag("div");
-					String[] names = div_tags.get(1).ownText().split("<br>");
-					for (String name : names) {
-						result += name;
-					}
+					String[] names = div_tags.get(0).ownText().split("<br>");
+					result += names[0];
 				}
+
+				// 获取Exact mass
+				if (nobr_str.equals("Exact mass")) {
+					result += "\t";
+					Element other_tr = nobr.parent().nextElementSibling();
+					Elements div_tags = other_tr.getElementsByTag("div");
+					String[] names = div_tags.get(0).ownText().split("<br>");
+					result += names[0];
+				}
+
+				// 获取Mol weight
+				if (nobr_str.equals("Mol weight")) {
+					result += "\t";
+					Element other_tr = nobr.parent().nextElementSibling();
+					Elements div_tags = other_tr.getElementsByTag("div");
+					String[] names = div_tags.get(0).ownText().split("<br>");
+					result += names[0];
+				}
+
 			}
 		} catch (Exception e1) {
 			e1.printStackTrace();
@@ -69,12 +95,15 @@ public class GetDrugsFromKEGG {
 		String url = "";
 		String result = null;
 		while (id <= max) {
-			System.out.println(id);
 			url = "http://www.kegg.jp/dbget-bin/www_bget?dr:D" + df.format(id);
+			System.out.println(url);
 			result = getContentPr(url);
 			if (result != "") {
-				outputStream.write(("D" + df.format(id) + "\t"
-						+ getContentPr(url) + "\n").getBytes());
+				outputStream
+						.write(("D" + df.format(id) + getContentPr(url) + "\n")
+								.getBytes());
+				//注意flush
+				outputStream.flush();
 			}
 			id++;
 		}
